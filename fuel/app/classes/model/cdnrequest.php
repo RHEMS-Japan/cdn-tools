d7fa8111d46e842e160b334afc7736bcf2745960
hac <hac@192.168.59.3>
2015-06-01 12:44:24 +0900

    CDN統合によりモデルを整理

---
+++ fuel/app/classes/model/cdnrequest.php
@@ -0,0 +1,37 @@
+<?php
+
+class Model_CdnRequest extends Orm\Model {
+
+    protected static $_table_name = 'cdnrequest';
+    protected static $_properties = array(
+        'id',
+        'cdnType',
+        'estimatedSeconds',
+        'progressUri',
+        'purgeId',
+        'supportId',
+        'httpStatus',
+        'detail',
+        'pingAfterSeconds',
+        'done',
+        'created_at',
+        'updated_at',
+    );
+
+    /*
+      CREATE TABLE cdnrequest(
+      id integer primary key autoincrement,
+      cdnType text,
+      estimatedSeconds text,
+      progressUri text,
+      purgeId text,
+      supportId text,
+      httpStatus int,
+      detail text,
+      pingAfterSeconds int,
+      done int,
+      created_at int,
+      updated_at int
+      );
+     */
+}
