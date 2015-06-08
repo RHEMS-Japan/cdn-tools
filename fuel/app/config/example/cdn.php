ba227b0f44273308ac26def9430a993adb88a7f7
hac <hac@192.168.10.10>
2015-06-08 10:48:29 +0900

    ドキュメントに合わせたひな形設定に修正

--- fuel/app/config/example/cdn.php
+++ fuel/app/config/example/cdn.php
@@ -1,46 +1,31 @@
 <?php
+
 return array(
-   'akamai' => array(
-       'account1' => array(
-           'enable' => true,
-           'authentication' => array(
-               'user' => 'hogehoge@hogefuga.jp',
-               'password' => 'hogehoge',
-           ),
-           'notification' => array(
-               'type' => 'mail',
-               'addr' => 'hogehoge@hogefuga.jp',
-               'title' => 'report-akamai',
-           ),
-       ),
-   ),
-   'keycdn' => array(
-       'account1' => array(
-           'enable' => true,
-           'authentication' => array(
-               'user' => 'hogehoge@hogefuga.jp',
-               'password' => 'hogehoge',
-           ),           
-           'notification' => array(
-               'type' => 'slack',
-               'token' => 'XXXX-YYYY-XZZZZ-ZZXXYY-000-AAAA',
-               'title' => 'report-keycdn',
-               'channel' => 'cdn-channel',
-           ),
-       ),
-   ),
-   'cloudfront' => array(
-       'account1' => array(
-           'enable' => true,
-           'authentication' => array(
-               'user' => '<AccessKey>',
-               'password' => '<SecretKey>',
-           ),           
-           'notification' => array(
-               'type' => 'hipchat',
-               'token' => '<HipChat v2 API token>',
-               'target' => '@user or room-id',
-           ),
-       ),
-   ),
+    'akamai' => array(
+        'account1' => array(
+            'authentication' => array(
+                'user' => 'hogehoge@hogefuga.jp',
+                'password' => 'hogehoge',
+            ),
+            'notification' => array(
+                'type' => 'hipchat',
+                'token' => '<HipChat v2 API token>',
+                'room' => '@user or room-id',
+            ),
+        ),
+    ),
+    'keycdn' => array(
+        'account1' => array(
+            'authentication' => array(
+                'user' => 'hogehoge@hogefuga.jp',
+                'password' => 'hogehoge',
+            ),
+            'notification' => array(
+                'type' => 'slack',
+                'token' => 'xoxp-xxxxxxxxx-eeeeee-yyyyyyy-zzzzzz',
+                'team' => 'Team',
+                'channel' => 'cdn-channel',
+            ),
+        ),
+    ),
 );
