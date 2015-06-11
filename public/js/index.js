4817400c1ee01d66645d5475c90e9313db47fc68
hac <hac@192.168.10.10>
2015-06-11 14:17:47 +0900

    ng-app及びng-controllerの追加

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
 var cdntools = angular.module('cdntools', ['ui.bootstrap']);
+cdntools.controller('cdntools_top', function ($scope, $q, $window) {
+   $scope.redirect = function(a,b) {
+       console.log(a);
+       console.log(b);
+       $window.location.href = '/cdn/' + a + '/' + b;
+   };
+});
