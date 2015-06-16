ca9e7172acdfbe14555575731420c18b823bfcd3
hac <hac@192.168.10.4>
2015-06-16 18:32:23 +0900

    通知処理をTaskから分離

---
+++ fuel/app/classes/cdn/notification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cdn;
+
+use Config;
+use Messaging\Hipchat;
+use Messaging\Slack;
+
+class Notification {
+    function __construct($config, $msgs) {
+        $result = array(
+            'success' => false,
+        );
+        $token = $config['token'];
+        switch ($config['type']) {
+            case 'slack':
+                $channel = $config['channel'];
+                $msg_api = new Slack($token);
+                $result = $msg_api->send_message($channel, $msgs);
+                break;
+            case 'hipchat':
+                $room = $config['room'];
+                $msg_api = new Hipchat($token);
+                $result = $msg_api->send_message($room, $msgs);
+                break;
+        }
+        return $result;
+    }
+
+    
+}
+
